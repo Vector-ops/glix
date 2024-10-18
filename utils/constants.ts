572df307316642ccaecad7f337cc4c1d25a9dd82
Vector-ops <sumith2827@gmail.com>
2024-10-19 00:19:17 +0530

    chore: add constants

---
+++ utils/constants.ts
@@ -0,0 +1,15 @@
+export const Days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export const Months = [
+	"January",
+	"February",
+	"March",
+	"April",
+	"May",
+	"June",
+	"July",
+	"August",
+	"September",
+	"November",
+	"December",
+];
