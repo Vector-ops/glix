c361ea826fb6821e48243c46c62ec5a69d1b79d7
Vector-ops <sumith2827@gmail.com>
2024-10-19 00:17:30 +0530

    feat: add button component

---
+++ components/Button.tsx
@@ -0,0 +1,18 @@
+import { PaperPlaneTilt } from "@phosphor-icons/react";
+import Link from "next/link";
+import React from "react";
+
+const Button = () => {
+	return (
+		<div
+			className="w-16 h-10 border border-gray-500 
+		bg-white dark:bg-gray-900 dark:border-stone-300 rounded-xl flex justify-center items-center hover:bg-stone-300 dark:hover:bg-stone-800 cursor-pointer dark:hover:text-white"
+		>
+			<Link href="infernorift44@gmail.com">
+				<PaperPlaneTilt weight="fill" />
+			</Link>
+		</div>
+	);
+};
+
+export default Button;
