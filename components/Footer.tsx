ba99c56379475740d25336580aeba0fffafcad5a
Vector-ops <sumith2827@gmail.com>
2024-10-19 00:17:38 +0530

    feat: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+	const date = new Date();
+	return (
+		<footer className="fixed bottom-0 right-0 left-0 flex flex-col justify-between items-center gap-2 w-full border-t border-gray-300 dark:border-gray-500 p-2 px-2 pb-2 bg-white dark:bg-[#1c1b22]">
+			<div className="flex justify-between items-center w-full mr-4">
+				<p className="text-sm font-bold">Glix</p>
+				<p className="text-sm">Made with ❤️ by Vector-ops</p>
+			</div>
+
+			<p className="text-sm">Copyright {date.getFullYear()}</p>
+		</footer>
+	);
+};
+
+export default Footer;
