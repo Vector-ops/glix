65ccb0d8cd824eb9d101f8131816e637e1c750c1
Vector-ops <sumith2827@gmail.com>
2024-10-19 12:38:03 +0530

    chore: add mailto to link

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ const Button = () => {
 			className="w-16 h-10 border border-gray-500 
 		bg-white dark:bg-gray-900 dark:border-stone-300 rounded-xl flex justify-center items-center hover:bg-stone-300 dark:hover:bg-stone-800 cursor-pointer dark:hover:text-white"
 		>
-			<Link href="infernorift44@gmail.com">
+			<Link href="mailto:infernorift44@gmail.com" target="_blank">
 				<PaperPlaneTilt weight="fill" />
 			</Link>
 		</div>
