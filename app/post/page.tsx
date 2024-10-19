07c0cfa998b4e6be3163df6c8e6889eae194fd9e
Vector-ops <sumith2827@gmail.com>
2024-10-19 11:40:13 +0530

    refactor: order posts based on time

--- app/post/page.tsx
+++ app/post/page.tsx
@@ -1,11 +1,27 @@
+"use client";
+
 import { posts } from "@/.velite";
 import PostCard from "@/components/PostCard";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { IPost } from "../types";
 
 const page = () => {
+	const [orderedPosts, setOrderedPosts] = useState<IPost[]>();
+
+	useEffect(() => {
+		const sortedPosts = posts.sort((a, b) => {
+			const dateA = new Date(a.date);
+			const dateB = new Date(b.date);
+
+			return dateA > dateB ? -1 : 1;
+		});
+
+		setOrderedPosts(sortedPosts);
+	}, [posts]);
+
 	return (
 		<div className="flex flex-col justify-center items-center gap-4 w-full m-4 flex-wrap">
-			{posts.map((post, index) => (
+			{orderedPosts?.map((post, index) => (
 				<PostCard post={post} key={index} />
 			))}
 		</div>
