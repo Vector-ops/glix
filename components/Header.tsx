ebdb069f3de5f2823ea7a0a18fffff983cf22109
Vector-ops <sumith2827@gmail.com>
2024-10-20 12:36:40 +0530

    refactor: add links to home search and posts

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Article, House, MagnifyingGlass } from "@phosphor-icons/react";
 import Link from "next/link";
 import React from "react";
 import { ThemeToggle } from "./ThemeToggle";
@@ -11,7 +12,18 @@ const Header = () => {
 				<Link href="/" className="text-3xl font-bold text-red-400">
 					Glix
 				</Link>
-				<ThemeToggle />
+				<div className="flex justify-center items-center gap-8">
+					<Link href="/" className="hover:text-red-400">
+						<House size={20} />
+					</Link>
+					<Link href="/post" className="hover:text-red-400">
+						<Article size={20} />
+					</Link>
+					<Link href={"/search"} className="hover:text-red-400">
+						<MagnifyingGlass size={20} />
+					</Link>
+					<ThemeToggle />
+				</div>
 			</header>
 		</div>
 	);
